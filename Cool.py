b4a447deecf17de58a789fe90f8b77f5bd5bd5ff
vanessa-miller <vanessamiller101@gmail.com>
2026-08-23 14:17:39 +0800

    Update Cool.py

--- Cool.py
+++ Cool.py
@@ -1,1 +1,23 @@
-print(hello team 3 slay)
+#include <Audio.h>
+#include <Wire.h>
+
+AudioInputI2S     i2sIn;
+AudioRecordQueue  queue1;
+AudioConnection   patchCord1(i2sIn, 0, queue1, 0); // left channel
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  AudioMemory(8);
+  queue1.begin();
+}
+
+void loop() {
+  if (queue1.available() >= 1) {
+    int16_t *data = queue1.readBuffer();
+    for (int i = 0; i < 128; i++) {
+      if (data[i] != 0) Serial.println(data[i]);
+    }
+    queue1.freeBuffer();
+  }
+}
