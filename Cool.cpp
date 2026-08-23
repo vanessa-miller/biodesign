bbc089dca4f7af650958a1446234290721733550
vanessa-miller <vanessamiller101@gmail.com>
2026-08-23 16:27:54 +0800

    Update Cool.cpp - claude code for printing all audio inputs

--- Cool.cpp
+++ Cool.cpp
@@ -1,23 +1,44 @@
 #include <Audio.h>
-#include <Wire.h>
+
+const int ledPin = LED_BUILTIN;
+const int soundThreshold = 2000; // tune this if needed for your mic level
 
 AudioInputI2S     i2sIn;
 AudioRecordQueue  queue1;
-AudioConnection   patchCord1(i2sIn, 0, queue1, 0); // left channel
+AudioConnection patchCord1(i2sIn, 1, queue1, 0); // try channel 1
 
 void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial) {
+    ;
+  }
+
   AudioMemory(8);
   queue1.begin();
+
+  Serial.println("Microphone audio trigger ready");
 }
 
 void loop() {
   if (queue1.available() >= 1) {
     int16_t *data = queue1.readBuffer();
+    int maxAmplitude = 0;
+
     for (int i = 0; i < 128; i++) {
-      if (data[i] != 0) Serial.println(data[i]);
+      int sampleAbs = abs(data[i]);
+      if (sampleAbs > maxAmplitude) {
+        maxAmplitude = sampleAbs;
+      }
     }
+
     queue1.freeBuffer();
+
+    Serial.println(maxAmplitude); // ALWAYS print, for calibration
+
+    bool soundDetected = (maxAmplitude > soundThreshold);
+    digitalWrite(ledPin, soundDetected ? HIGH : LOW);
   }
 }
