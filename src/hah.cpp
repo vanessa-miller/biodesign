97fb520be7347a67c296d2df0fe7e5c3ece5936f
movinlikejimmy <marshalljames258@gmail.com>
2026-08-23 15:47:36 +0800

    Add microphone sound trigger sketch

--- src/hah.cpp
+++ src/hah.cpp
@@ -1,23 +1,48 @@
 #include <Audio.h>
-#include <Wire.h>
+
+const int ledPin = LED_BUILTIN;
+const int soundThreshold = 2000; // tune this if needed for your mic level
 
 AudioInputI2S     i2sIn;
 AudioRecordQueue  queue1;
 AudioConnection   patchCord1(i2sIn, 0, queue1, 0); // left channel
 
 void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial) {
+    ;
+  }
+
   AudioMemory(8);
   queue1.begin();
+
+  Serial.println("Microphone audio trigger ready");
 }
 
 void loop() {
   if (queue1.available() >= 1) {
     int16_t *data = queue1.readBuffer();
+    int maxAmplitude = 0;
+
     for (int i = 0; i < 128; i++) {
-      if (data[i] != 0) Serial.println(data[i]);
+      int sample = data[i];
+      int sampleAbs = abs(sample);
+      if (sampleAbs > maxAmplitude) {
+        maxAmplitude = sampleAbs;
+      }
     }
+
     queue1.freeBuffer();
+
+    bool soundDetected = (maxAmplitude > soundThreshold);
+    digitalWrite(ledPin, soundDetected ? HIGH : LOW);
+
+    if (soundDetected) {
+      Serial.print("Sound detected! Peak amplitude: ");
+      Serial.println(maxAmplitude);
+    }
   }
 }
